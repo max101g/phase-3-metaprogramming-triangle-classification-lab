40a931e214869f24e5a24ee73a73d09c5ecbe135
max101g <maxgithinji101@gmail.com>
2023-02-13 22:14:36 +0300

    Ruby commit

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @sides.any? {|side| side <= 0} || @sides.max >= @sides.inject(:+) - @sides.max
+      raise TriangleError
+    elsif @sides.uniq.size == 1
+      :equilateral
+    elsif @sides.uniq.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+
+  end
+
+class TriangleError < StandardError
+end
+
 end
